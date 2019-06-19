e2a918e138077ad703e58e968b644bb4bd21ed0d
Tanner Rogalsky <tanner@tannerrogalsky.com>
2019-06-19 18:41:21 -0400

    Revert "session reference sharing working"
    This reverts commit 4a819783d79aef3b96f3641b4a2dab7be05802c3.

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,16 @@
 extern crate ws;
 extern crate serde_json;
 extern crate redis;
-use redis::{Commands, RedisError};
-
-extern crate serde;
-use serde::{Serialize, Deserialize};
+use redis::Commands;
 
 mod message;
 use message::Message;
-use std::collections::HashMap;
-use std::collections::hash_map::Entry;
-use std::cell::RefCell;
-use std::rc::Rc;
 
-#[derive(Serialize, Deserialize)]
-struct RelayServerToken {
-    #[serde(rename = "sessionStartTime")]
-    session_start_time : i64,
-}
-
-struct Session {
-    id: message::RelayKey,
-//    client: Option<ws::Sender>,
-//    server: ws::Sender,
-//    established: bool,
-//    verified: bool,
-//    session_start_time: i64,
-}
-
-type Sessions = Rc<RefCell<HashMap<message::RelayKey, Session>>>;
-
-struct Server<'a> {
-    redis: &'a redis::Connection,
-    sessions: Sessions,
+struct Server {
     out: ws::Sender,
 }
 
-//impl<'a> Server<'a> {
-//    fn get_or_create_session(&mut self, session_key: &message::RelayKey) -> &mut Session {
-////        match self.sessions.get(session_key) {
-////            Some(session) => Ok(session),
-////            None => {
-////                let redis_result : Result<String, RedisError> = redis::pipe().atomic()
-////                    .cmd("GET").arg("bvg-streaming-relay-".to_owned() + session_key)
-////                    .cmd("DEL").arg("bvg-streaming-relay-".to_owned() + session_key)
-////                    .query(self.redis);
-////                match redis_result {
-////                    Err(err) => Err(err.to_string()),
-////                    Ok(redis_key) => {
-////                        println!("{:?}", redis_key);
-////                        let new_session = Session {
-////                            id: session_key.to_owned(),
-////                        };
-////                        self.sessions.insert(session_key.to_owned(), new_session);
-////                        Ok(&new_session)
-////                    },
-////                }
-////            },
-////        }
-//
-////        match self.sessions.borrow_mut().entry(session_key.to_owned()) {
-////            Entry::Occupied(entry) => Ok(entry.get()),
-////            Entry::Vacant(_) => Err("oh no".to_owned()),
-////        }
-//
-//        self.sessions.borrow_mut().entry(session_key.to_owned()).or_insert_with(|| Session { id: session_key.to_owned() })
-//    }
-//}
-
-impl<'a> ws::Handler for Server<'a> {
+impl ws::Handler for Server {
     fn on_open(&mut self, shake: ws::Handshake) -> ws::Result<()> {
         if let Some(ip_addr) = shake.remote_addr()? {
             println!("Connection opened from {}.", ip_addr)
@@ -88,12 +30,6 @@ impl<'a> ws::Handler for Server<'a> {
                     Message::Ping {} => self.out.send(serde_json::to_string(&Message::Pong {}).unwrap()),
                     Message::ClientRegister {data} => {
                         println!("{:?}", data.key);
-//                        let _session = self.get_or_create_session(&data.key);
-                        let _session = self.sessions.borrow_mut().entry(data.key.to_owned()).or_insert_with(|| {
-                            Session {
-                                id: data.key,
-                            }
-                        });
                         Ok(())
                     },
                     _ => self.out.close(ws::CloseCode::Unsupported),
@@ -115,14 +51,8 @@ fn main() {
     let r : i32 = con.get("my_key").unwrap();
     println!("{}", r);
 
-    let sessions = Rc::new(RefCell::new(HashMap::new()));
-
     if let Err(error) = ws::listen("127.0.0.1:3012", |out| {
-        Server {
-            redis: &con,
-            sessions: sessions.clone(),
-            out,
-        }
+        Server { out }
     }) {
         println!("Failed to create WebSocket due to {:?}", error);
     }
